c0d39e6c90f24a8894f97d83d6758644c26774d2
mayanktyagi <mayanktyagi1709@gmail.com>
2020-06-18 08:53:21 +0530

    First commit

---
+++ memory.js
@@ -0,0 +1,10 @@
+/*const cards = document.querySelectorAll(".memory-card");
+
+function flipcard(){
+	console.log("working");
+	console.log(this);
+}
+
+cards foreach(card=> card.addEventListener('click', flipcard))*/
+
+alert("hi");
